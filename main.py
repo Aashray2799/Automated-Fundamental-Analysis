e6422a6c5ed033a0fc896d5ae2d790a6e6393b95
Aashray2799 <nikhil.aashray@gmail.com>
2025-04-05 17:39:39 -0700

    main.py

--- main.py
+++ main.py
@@ -12,9 +12,7 @@ st.markdown("""
 Welcome to **StockViz**, a web app for analyzing and scoring 8,000+ U.S. stocks based on key financial metrics.
 """)
 st.markdown("""
-📂 View the file `StockRatings-04.05.22.csv` as an example of the output of this program.
-
-**Data Source**: Finviz  
+Powered by market data to help visualize how stocks compare in valuation, growth, and profitability. 
 """)
 
 # Load dataset
